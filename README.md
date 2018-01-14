b548936bd4dddfc19564b90275a8d38a5e6ae6a3
Eric Zhang <ekzhang1@gmail.com>
2018-01-13 22:00:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+music-gen
+=========
+
+Generate and play music with a recurrent neural network running in the browser.
+
+Built using keras.js and abcjs.
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const s = `X: 8
+T:Ftrhwls Hadkyeas
+% Nottingham Music Database
+S:Trad, arr Phil Rowe
+M:6/8
+K:A
+"E7"ABd |[2"A"e2e e2e|"D7"d2B "A"c2A|"Bm"B2B "A7"EBc|"Bm"d2B "A7"ABG|
+"D"F2D A2A|"G"B2G "A7"B2B|"D"A2F F2G|"Em"GAG "A7"A2G|
+"D"d2A Bcd|"G"efg "A7"edc|"Em"Bee "A7"cBA|"D"A2c d2:|`
+
+ABCJS.renderAbc('sheet-music', s);
