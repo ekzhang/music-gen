0c2910d339e44638019d505a50ab135f83e59c87
Eric Zhang <ekzhang1@gmail.com>
2018-01-15 02:22:39 -0600

    completely revamp styling

--- docs/index.js
+++ docs/index.js
@@ -1,35 +1,53 @@
 ABCJS.midi.soundfontUrl = "https://gleitz.github.io/midi-js-soundfonts/MusyngKite/";
 
+const btn = document.getElementById('compose');
+const message = document.getElementById('loading-message');
+const container = document.getElementById('loading-container');
+
 function display(song) {
   ABCJS.renderAbc('sheet-music', song);
   ABCJS.renderMidi('midi-music', song);
 }
 
+function displayClear() {
+  document.getElementById('sheet-music').innerHTML = '';
+  document.getElementById('midi-music').innerHTML = '';
+}
+
+function showLoading(msg) {
+  btn.style.display = 'none';
+  message.innerHTML = msg;
+  container.style.display = 'block';
+}
+
+function clearLoading() {
+  btn.style.display = 'block';
+  message.innerHTML = '';
+  container.style.display = 'none';
+}
+
 async function main() {
   const worker = new Worker('worker.js');
-
-  const btn = document.getElementById('compose');
-  const message = document.getElementById('message');
+  showLoading('Loading model...');
 
   worker.onmessage = function(e) {
     const d = e.data;
     if (d.type === 'ready') {
-      message.innerHTML = '';
-
-      btn.onclick = async function () {
-        btn.disabled = true;
-        message.innerHTML = 'Composing...';
-        worker.postMessage({ type: 'sample' });
-      }
+      clearLoading();
     }
     else if (d.type === 'song') {
       display(d.payload);
-      message.innerHTML = '';
-      btn.disabled = false;
+      clearLoading();
     }
     else {
       console.error('Invalid message from worker: ' + d);
     }
+  }
+
+  btn.onclick = async function () {
+    displayClear();
+    showLoading('Composing...');
+    worker.postMessage({ type: 'sample' });
   }
 }
 
